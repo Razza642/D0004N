d4254729060015606e3eb40cdb0849bca7f531c8
Razza642 <148590345+Razza642@users.noreply.github.com>
2023-10-20 23:11:51 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -107,6 +107,44 @@ INSERT INTO hotell.bokning_res(
     0
 );
 
+---------------------------------------------
 
+SELECT * FROM hotell.gast
+ORDER BY namn; 
+-- sorering i alfabetsordning
+
+INSERT INTO hotell.gast 
+VALUES ("Rasmus","Teo"),
+("123123123","987654321"),
+("raswid-3@student.ltu.se",
+"teolas-1@student.ltu.se"),
+("1","0"),("Piteå","Luleå")
+;
+-- flera gäster registrerade samtidigt
+
+SELECT count(*) from bokning_rum 
+WHERE date BETWEEN '2023-06-01' AND '2023-08-31';
+-- hur mång bokningar som gjorts under sommarperioden
+
+SELECT rum.rumnr, count (bokning_rum.bokdatum) from rum
+JOIN bokning_gaster
+ON rum.rum_nr = bokning_gaster.rum_nr
+JOIN in_ut_checkning
+ON bokning_gaster.bg_id = in_ut_checkning.bk_id
+WHERE date BETWEEN '2023-06-01' AND '2023-08-31';
+-- vilka rum som är mest populära under sommarperioden (är nog ute och cyklar)
+
+UPDATE anstallda
+SET address = "Björkvägen 6"
+WHERE anst_id = "199903134387"
+-- uppdatering av personals address
+
+INSERT INTO erbjudande
+VALUES ("924173006488", "9", "2023-06-23","2023-06-25","500");
+-- nytt erbjudande för midsommarhelgen
+
+SELECT count(gast_antal) from bokning_res
+ORDER BY datum
+WHERE datum BETWEEN '2023-07-01' AND '2023-07-31';
 
 
